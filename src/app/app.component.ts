ccc6ab51e960addd8e2293b72cce29dcb563a8c7
vijay <svj141@gmail.com>
2023-05-13 23:42:00 -0400

    JT-01 mock drag and drop feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'job-tracker-app';
+
+  drag(ev: any) {
+    ev.dataTransfer.setData('text', ev.target.id);
+  }
+
+  allowDrop(ev: any) {
+    ev.preventDefault();
+  }
+
+  drop(ev: any) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData('text');
+    ev.target.appendChild(document.getElementById(data));
+  }
 }
